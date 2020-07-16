ff2842844f81eb914c8c0dc1892590f0907155b2
Alexander Primakov <primakovalexander07041993@gmail.com>
2020-07-17 01:47:36 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,47 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User getUserByCar(Long id, int series) {
+      Car car = (Car) sessionFactory
+              .getCurrentSession()
+              .createQuery("from Car where id = :id and series = :series")
+              .setParameter("id", id)
+              .setParameter("series", series)
+              .getSingleResult();
+      return (User) sessionFactory.getCurrentSession()
+              .createQuery("from User where car = :car")
+              .setParameter("car", car)
+              .setMaxResults(1)
+              .getSingleResult();
+   }
+
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,54 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "series")
+    private int series;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(int series, String name) {
+        this.series=series;
+        this.name = name;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
